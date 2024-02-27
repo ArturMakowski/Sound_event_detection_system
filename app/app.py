19495a607b753ea25f3aeeed57db9c2548852f45
Artur Makowski <artmak1@st.amu.edu.pl>
2024-02-27 21:28:36 +0100

    add: streamlit app

---
+++ app/app.py
@@ -0,0 +1,139 @@
+import base64
+
+import sys
+from io import BytesIO
+
+import dcase_util
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sed_vis
+import streamlit as st
+import torch
+import yaml
+from PIL import Image
+
+
+sys.path.append("../")
+from desed_task.dataio.datasets import StronglyAnnotatedSet
+from encoder import ManyHotEncoder
+from model import CRNN
+from trainer import SED
+from utils import batched_decode_preds, classes_labels
+
+
+def inference(
+    idx, model_path="../dvclive/artifacts/epoch=68-step=7314.ckpt", print_df=False
+):
+    with open("../params.yaml", "r") as f:
+        config = yaml.safe_load(f)
+
+    encoder = ManyHotEncoder(
+        list(classes_labels.keys()),
+        audio_len=config["data"]["audio_max_len"],
+        frame_len=config["feats"]["n_filters"],
+        frame_hop=config["feats"]["hop_length"],
+        net_pooling=config["data"]["net_subsample"],
+        fs=config["data"]["fs"],
+    )
+
+    sed = SED(config, encoder=encoder, sed=CRNN(**config["net"]))
+    # print(sed.state_dict().keys())
+
+    state_dict = torch.load(model_path, map_location=torch.device("cpu"))["state_dict"]
+    sed.load_state_dict(state_dict)
+
+    tsv_entries_strong = pd.read_csv(config["data"]["test_tsv"], sep="\t")
+    dataset_strong = StronglyAnnotatedSet(
+        audio_folder=config["data"]["test_folder"],
+        tsv_entries=tsv_entries_strong,
+        encoder=encoder,
+        return_filename=True,
+        pad_to=config["data"]["audio_max_len"],
+    )
+
+    preds = batched_decode_preds(
+        sed(dataset_strong[idx][0]), dataset_strong[idx][3], encoder
+    )
+    # print(dataset_strong[i][3])
+    preds[2][0.5][["onset", "offset", "event_label"]].to_csv(
+        "preds.tsv", index=False, sep="\t"
+    )
+
+    if print_df:
+        print("--------Preds--------")
+        print(preds[2][0.5][["onset", "offset", "event_label"]])
+
+    ref_df = pd.read_csv(config["data"]["test_tsv"], sep="\t")
+    ref_df = (
+        ref_df[ref_df["filename"] == dataset_strong[idx][3].split("/")[-1]]
+        .reset_index()
+        .drop(columns=["index"])
+    )
+    ref_df.drop("filename", axis=1).to_csv("ref.tsv", index=False, sep="\t")
+
+    if print_df:
+        print("--------Ref--------")
+        print(ref_df)
+
+    audio_container = dcase_util.containers.AudioContainer().load(
+        f"/mnt/d/DESED_dataset/audio/validation/validation_16k/{dataset_strong[idx][3].split('/')[-1]}"
+    )
+    audio_container.data = np.mean(audio_container.data, axis=0)
+
+    # Load event lists
+    reference_event_list = dcase_util.containers.MetaDataContainer().load("ref.tsv")
+    estimated_event_list = dcase_util.containers.MetaDataContainer().load("preds.tsv")
+
+    event_lists = {"reference": reference_event_list, "estimated": estimated_event_list}
+
+    # Visualize the data
+    vis = sed_vis.visualization.EventListVisualizer(
+        event_lists=event_lists,
+        audio_signal=audio_container.data,
+        sampling_rate=audio_container.fs,
+        publication_mode=True,
+    )
+
+    return vis, audio_container.data, audio_container.fs
+
+
+def get_image_download_link(vis):
+    """Generates a link allowing the data in a given plot to be downloaded"""
+    buffer = BytesIO()
+    vis.savefig(buffer, format="png")
+    buffer.seek(0)
+    image_png = buffer.getvalue()
+    b64 = base64.b64encode(image_png).decode()
+    return f'<a href="data:image/png;base64,{b64}" download="plot.png">Download plot as png</a>'
+
+
+def main():
+    st.title("SED Visualization App")
+
+    idx = st.number_input("Enter the index:", value=5)
+
+    try:
+        vis, *_ = inference(idx)
+    except IndexError:
+        st.error("Invalid index. Please enter a valid index.")
+        return
+
+    vis.save(filename="vis.png")
+
+    # Open the image file
+    img = Image.open("vis.png")
+
+    # Convert the image to base64
+    with open("vis.png", "rb") as img_file:
+        b64_string = base64.b64encode(img_file.read()).decode()
+
+    # Display the image using markdown and center it
+    st.markdown(
+        f'<div style="display: flex; justify-content: center;"><img src="data:image/png;base64,{b64_string}"/></div>',
+        unsafe_allow_html=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
